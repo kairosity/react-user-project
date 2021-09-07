8724d34f7db0a3e190601fda409dc86d1bbf146e
Karina Finegan <kairosity@users.noreply.github.com>
2021-09-07 17:25:52 +0100

    app finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import AddUser from './components/Users/AddUser';
+import UserList from './components/Users/UserList';
 
 
 function App() {
+  const [userList, setUserList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUserList((prevUserList) => {
+      return [...prevUserList, {id: Math.random().toString(), name: uName, age: uAge}];
+    });
+  };
+
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler}  />
+      <UserList users={userList}/>
     </div>
   );
 }
--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -4,7 +4,13 @@ import classes from './Button.module.css'
 
 const Button = (props) => {
     return (
-        <div className={`${classes.button} ${props.className}`}>{props.children}</div>
+        <button 
+            className={`${classes.button} ${props.className}`}
+            type={props.type || 'button'}
+            onClick={props.onClick}
+        >
+            {props.children}
+        </button>
     );
 };
 
